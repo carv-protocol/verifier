8f7c1ae3fd43b540fa0b9d3c1920c987b72218a4
shanshanramondy <yuanshanshan33521@gmail.com>
2024-04-19 17:04:20 +0800

    1. v1 -> mrEnclave verify finish 2. teeAddress get from config.yaml

--- internal/worker/chain.go
+++ internal/worker/chain.go
@@ -4,8 +4,6 @@ import (
 	"context"
 	"crypto/ecdsa"
 	"encoding/hex"
-	"fmt"
-	"github.com/carv-protocol/verifier/pkg/attestion"
 	"math/big"
 	"os"
 	"strings"
@@ -24,6 +22,7 @@ import (
 	"github.com/carv-protocol/verifier/internal/biz"
 	"github.com/carv-protocol/verifier/internal/conf"
 	"github.com/carv-protocol/verifier/internal/data"
+	"github.com/carv-protocol/verifier/pkg/attestion"
 	"github.com/carv-protocol/verifier/pkg/contract"
 )
 
@@ -306,7 +305,6 @@ func (c *Chain) verifyAttestation(ctx context.Context, attestationIds [][32]byte
 
 	tx, err := c.contractObj.VerifyAttestationBatch(auth, attestationIds, results)
 	if err != nil {
-		fmt.Println("txError: ", err)
 		return txHash, errors.Wrap(err, "contract VerifyAttestationBatch error")
 	}
 	txHash = tx.Hash().Hex()
@@ -375,7 +373,7 @@ func (c *Chain) process(ctx context.Context, startBlockNumber, endBlockNumber in
 		attestationIds = append(attestationIds, unpackedData.AttestationId)
 		// attestation mrenclave
 
-		_, mrEnclaveFronContract, err := c.contractObj.GetTeeInfo(&bind.CallOpts{}, common.HexToAddress("0x19baa72643aa11b28cb6251fd7596201778ead9a"))
+		_, mrEnclaveFronContract, err := c.contractObj.GetTeeInfo(&bind.CallOpts{}, common.HexToAddress(c.cf.Contract.TeeAddr))
 		if err != nil {
 			return errors.Wrap(err, "contract GetTeeInfo error")
 		}
