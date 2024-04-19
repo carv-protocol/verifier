6e4bae81dcc4073723e1ee6e764c6b75f5fe177a
shanshanramondy <yuanshanshan33521@gmail.com>
2024-04-19 18:07:28 +0800

    1. sm4.go 2. rename

---
+++ internal/worker/sm4.go
@@ -0,0 +1,50 @@
+package worker
+
+import (
+	"bytes"
+	"crypto/cipher"
+	"encoding/hex"
+	"github.com/tjfoc/gmsm/sm4"
+)
+
+func Sm4Encrypt(key, iv, plainText []byte) ([]byte, error) {
+	block, err := sm4.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+	blockSize := block.BlockSize()
+	origData := pkcs5Padding(plainText, blockSize)
+	blockMode := cipher.NewCBCEncrypter(block, iv)
+	cryted := make([]byte, len(origData))
+	blockMode.CryptBlocks(cryted, origData)
+	return cryted, nil
+}
+
+func Sm4Decrypt(c *Chain) ([]byte, error) {
+	key := []byte(c.cf.Wallet.Key)
+	iv := []byte(c.cf.Wallet.Iv)
+	private_encode := c.cf.Wallet.PrivateEncode
+	cipherText, err := hex.DecodeString(private_encode)
+	block, err := sm4.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+	blockMode := cipher.NewCBCDecrypter(block, iv)
+	origData := make([]byte, len(cipherText))
+	blockMode.CryptBlocks(origData, cipherText)
+	origData = pkcs5UnPadding(origData)
+	return origData, nil
+}
+
+// pkcs5 padding
+func pkcs5Padding(src []byte, blockSize int) []byte {
+	padding := blockSize - len(src)%blockSize
+	padtext := bytes.Repeat([]byte{byte(padding)}, padding)
+	return append(src, padtext...)
+}
+
+func pkcs5UnPadding(src []byte) []byte {
+	length := len(src)
+	unpadding := int(src[length-1])
+	return src[:(length - unpadding)]
+}
