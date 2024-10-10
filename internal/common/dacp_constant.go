b618482d53ffbb4851f6e6c07a70f9a59dc881be
Ambero Tu <ytu@carv.io>
2024-10-10 14:37:44 +0700

    update dacp config

--- internal/common/dacp_constant.go
+++ internal/common/dacp_constant.go
@@ -1,6 +1,6 @@
 package common
 
-const BASE_URl = "https://raw.githubusercontent.com/carv-protocol/verifier/verifier-alphanet/configs/"
+const BASE_URl = "https://raw.githubusercontent.com/carv-protocol/verifier/refs/heads/main/configs/"
 const IDENTITY_JSON = "identity.json"
 const TCB_JSON = "tcb.json"
 const TRUSTED_PATH = "trusted.json"
